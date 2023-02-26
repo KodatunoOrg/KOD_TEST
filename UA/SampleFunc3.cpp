e9bf07501108c33ec89e74998b38a56d5213011b
K-Magara <magara@maizuru-ct.ac.jp>
2023-02-26 11:28:34 +0900

    SampleFunc3.cpp

--- UA/SampleFunc3.cpp
+++ UA/SampleFunc3.cpp
@@ -1,5 +1,6 @@
 ﻿#include "StdAfxQt.h"
 #include "SampleFunc.h"
+#include "boost/foreach.hpp"
 
 // Function: Smp3xCAM
 // スキャンパスを生成するシンプルな3xCAM
@@ -13,13 +14,11 @@
 // D - Z方向分割数（粗加工用）
 void Smp3xCAM(NURBSS *S,NURBSC *C,double R,int N,double H,int D)
 {
-    NURBS_Func	nf;                 // NURBS_Funcへのインスタンス
     double green[3] = {0,1,0};      // 描画するパスの色（緑）
     Coord plane_pt;                 // 分割する平面上の1点
     Coord plane_n;                  // 分割する平面の法線ベクトル
-    Coord path_[2000];              // 一時格納用バッファ
-    Coord ***path = NewCoord3(D+1,N+1,2000);    // 生成されたパスを格納
-    int ptnum[100];                 // スキャンライン1本ごとの加工点数を格納
+	VVCoord vvPath;
+    VVVCoord vvvPath;				// 生成されたパスを格納
     int flag = 0;                   // ジグザグパス生成時の方向転換用フラグ
 
     // ガイドカーブに沿って垂直平面をシフトしていき，加工面との交点群を求めていく
@@ -27,34 +26,39 @@ void Smp3xCAM(NURBSS *S,NURBSC *C,double R,int N,double H,int D)
         double t = (double)i/(double)N;
 		if(i==0) t += 0.0001;		// 特異点回避
 		else if(i==N) t-= 0.0001;	// 特異点回避
-        plane_pt = nf.CalcNurbsCCoord(C,t);     // 注目中の垂直平面上の1点
-        plane_n = nf.CalcTanVecOnNurbsC(C,t);   // 注目中の垂直平面の法線ベクトル
-        ptnum[i] = nf.CalcIntersecPtsPlaneSearch(S,plane_pt,plane_n,0.5,3,path_,2000,RUNGE_KUTTA);  // 交点群算出
+        plane_pt = C->CalcNurbsCCoord(t);     // 注目中の垂直平面上の1点
+        plane_n = C->CalcTanVecOnNurbsC(t);   // 注目中の垂直平面の法線ベクトル
+        VCoord vPath = S->CalcIntersecPtsPlaneSearch(plane_pt,plane_n,0.5,3,RUNGE_KUTTA);  // 交点群算出
         // 得られた交点群を，加工面法線方向に工具半径分オフセットさせた点を得る
-		for(int j=0;j<ptnum[i];j++){
-            Coord pt = nf.CalcNurbsSCoord(S,path_[j].x,path_[j].y);     // 工具コンタクト点
-            Coord n = nf.CalcNormVecOnNurbsS(S,path_[j].x,path_[j].y);  // 法線ベクトル
+		for(int j=0;j<vPath.size();j++){
+            Coord pt = S->CalcNurbsSCoord(vPath[j].x,vPath[j].y);     // 工具コンタクト点
+            Coord n = S->CalcNormVecOnNurbsS(vPath[j].x,vPath[j].y);  // 法線ベクトル
             if(n.z < 0)	n = n*(-1);         // 法線ベクトルの向き調整
-			path[D][i][j] = pt + n*R;		// 工具半径オフセット
+			vPath[j] = pt + n*R;		// 工具半径オフセット
 		}
+		vvPath.push_back(vPath);
 	}
 
 	// 粗加工パス生成
 	for(int i=0;i<D;i++){
-		for(int j=0;j<N+1;j++){
-			for(int k=0;k<ptnum[j];k++){
-				double del = (H - path[D][j][k].z)/(double)D;
+		VVCoord vv;
+		for(int j=0;j<vvPath.size();j++){
+			VCoord v;
+			for(int k=0;k<vvPath[j].size();k++){
+				double del = (H - vvPath[j][k].z)/(double)D;
 				double Z = H - del*(double)i;
-				path[i][j][k].SetCoord(path[D][j][k].x,path[D][j][k].y,Z);
+				v.push_back( Coord(vvPath[j][k].x, vvPath[j][k].y, Z) );
 			}
+			vv.push_back(v);
 		}
+		vvvPath.push_back(vv);
 	}
 
 	// path描画
-	for(int i=0;i<D+1;i++){
-		for(int j=0;j<N+1;j++){
-			for(int k=0;k<ptnum[j];k++){
-				DrawPoint(path[i][j][k],1,1,green);
+	for(int i=0;i<vvvPath.size();i++){
+		for(int j=0;j<vvvPath[i].size();j++){
+			for(int k=0;k<vvvPath[i][j].size();k++){
+				DrawPoint(vvvPath[i][j][k],1,1,green);
 			}
 		}
 	}
@@ -65,23 +69,25 @@ void Smp3xCAM(NURBSS *S,NURBSC *C,double R,int N,double H,int D)
 	fprintf(fp,"G92X0.0Y0.0Z0.0\n");
 	fprintf(fp,"S4000M03\n");
 	fprintf(fp,"F500\n");
-	for(int i=0;i<D+1;i++){
-		fprintf(fp,"G00X%.2lfY%.2lfZ%.2lf\n",path[i][0][0].x,path[i][0][0].y,ret);
-		for(int j=0;j<N+1;j++){
-			if(flag == 0){
-				for(int k=0;k<ptnum[j];k++){
-					fprintf(fp,"G01X%.2lfY%.2lfZ%.2lf\n",path[i][j][k].x,path[i][j][k].y,path[i][j][k].z);
+
+	BOOST_FOREACH(auto& vv, vvvPath) {
+		fprintf(fp,"G00X%.2lfY%.2lfZ%.2lf\n",vv[0][0].x,vv[0][0].y,ret);
+		BOOST_FOREACH(auto& v, vv) {
+			if ( flag == 0 ) {
+				BOOST_FOREACH(auto& p, v) {
+					fprintf(fp,"G01X%.2lfY%.2lfZ%.2lf\n",p.x,p.y,p.z);
 				}
 			}
-			else{
-				for(int k=ptnum[j]-1;k>=0;k--){
-					fprintf(fp,"G01X%.2lfY%.2lfZ%.2lf\n",path[i][j][k].x,path[i][j][k].y,path[i][j][k].z);
+			else {
+				BOOST_REVERSE_FOREACH(auto& p, v) {
+					fprintf(fp,"G01X%.2lfY%.2lfZ%.2lf\n",p.x,p.y,p.z);
 				}
 			}
 			flag = 1 - flag;
 		}
 		fprintf(fp,"G01Z%.2lf\n",ret);
 	}
+
 	fprintf(fp,"G01Z%.2lf\n",ret);
 	fprintf(fp,"M05\n");
 	fprintf(fp,"M30\n");
